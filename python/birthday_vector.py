84b7a96dc7748ee85d272289978a7ff5cfc4dc4a
Sam Rodriguez <hey@samrodriguez.me>
2023-03-31 12:13:07 -0500

    Update birthday_vector.py

--- python/birthday_vector.py
+++ python/birthday_vector.py
@@ -9,19 +9,22 @@ from vpython import *
 scene.background = color.white
 scene.width = 600
 scene.height = 600
-scene.forward = vector(-.5,-.3,-1)
+scene.forward = vector(-.5, -.3, -1)
 
-scene.caption= """To rotate "camera", drag with right button or Ctrl-drag.
+scene.caption = """To rotate "camera", drag with right button or Ctrl-drag.
 To zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.
   On a two-button mouse, middle is left + right.
 To pan left/right and up/down, Shift-drag.
 Touch screen: pinch/extend to zoom, swipe or two-finger rotate."""
 
-xaxis=cylinder(color=vector(1,0,0), pos=vector(0,0,0), axis=vector(10,0,0), radius=0.3)
-xlbl=label(pos=vector(11,0,0), text="X", color=color.red, opacity=0, height=30, box=0)
-yaxis=cylinder(color=color.green, pos=vector(0,0,0), axis=vector(0,10,0), radius=0.3)
-ylbl=label(pos=vector(0,11,0), text="Y", color=color.green, opacity=0, height=30, box=0)
-zaxis=cylinder(color=color.blue, pos=vector(0,0,0), axis=vector(0,0,10), radius=0.3)
-xlbl=label(pos=vector(0,0,11), text="Z", color=color.blue, opacity=0, height=30, box=0)
+xaxis =cylinder(color=vector(1, 0, 0), pos=vector(0, 0, 0), axis=vector(10, 0, 0), radius=0.3)
+xlbl = label(pos=vector(11, 0, 0), text="X", color=color.red, opacity=0, height=30, box=0)
+yaxis = cylinder(color=color.green, pos=vector(0, 0, 0), axis=vector(0, 10, 0), radius=0.3)
+ylbl = label(pos=vector(0, 11, 0), text="Y", color=color.green, opacity=0, height=30, box=0)
+zaxis = cylinder(color=color.blue, pos=vector(0, 0, 0), axis=vector(0, 0, 10), radius=0.3)
+xlbl = label(pos=vector(0, 0, 11), text="Z", color=color.blue, opacity=0, height=30, box=0)
 
-r = arrow(pos=vector(0,0,0), axis = vector(2,10,7), color=color.white, shaftwidth=0.5)
+#the numbers were chosen from Sam's birthdate, which is 05/31
+
+r = arrow(pos=vector(0, 0, 0), axis=vector(5, 3, 1), color=color.yellow, shaftwidth=0.5)
+rlbl = label(pos=vector(0, 0, 0), text="r = 5x+3y+1z", color=color.black, box=0)

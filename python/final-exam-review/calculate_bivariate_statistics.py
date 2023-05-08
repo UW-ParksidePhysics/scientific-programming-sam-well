9c4c5d79d9c721a315522f5678abd861d59e5622
Sam Rodriguez <hey@samrodriguez.me>
2023-05-08 14:53:22 -0500

    Update calculate_bivariate_statistics.py

--- python/final-exam-review/calculate_bivariate_statistics.py
+++ python/final-exam-review/calculate_bivariate_statistics.py
@@ -8,7 +8,7 @@ import scipy as sp
 import numpy as np
 
 
-def calculate_bivariate_statistics(data):
+def calculate_bivariate_statistics(data, axis=1):
     """
     Calculate statistical characteristics of a data set using SciPy's stats.describe function
     :param data: ndarray, shape(2, M)
@@ -18,14 +18,17 @@ def calculate_bivariate_statistics(data):
     """
     # data = read_two_columns_text
     try:
-        statistics = sp.stats.describe(data)
+        statistics = sp.stats.describe(data, axis)
+        select_statistics = np.array([statistics.mean[1], statistics.variance[1], statistics.minmax[0][0],
+                                      statistics.minmax[1][0], statistics.minmax[0][1], statistics.minmax[1][1]])
     except IndexError as error:
         print(f'{error}')
-    return statistics
+    return select_statistics
 
 
 if __name__ == "__main__":
-    test_array = np.linspace(-10, 10, 21)
-    test_array = test_array ** 2
-    statistics = calculate_bivariate_statistics(test_array)
-    print(f'test_statistics={statistics}')
+    x_s = np.linspace(-10, 10, 21)
+    y_s = (x_s ** 2)
+    test_array = np.array([x_s, y_s])
+    select_statistics = calculate_bivariate_statistics(test_array)
+    print(f'test_statistics={select_statistics}')
